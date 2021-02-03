6b0eff2862a06d124b8ac7299ebb25c937209926
brandonspitza <77693884+brandonspitza@users.noreply.github.com>
2021-02-03 14:42:36 -0600

    Update Naturalight Home (Parent).groovy

--- Apps/Naturalight/Naturalight Home (Parent).groovy
+++ Apps/Naturalight/Naturalight Home (Parent).groovy
@@ -1,6 +1,6 @@
 /*
 
-    Hubitat Natrualight Home (Parent) v2.1
+    Hubitat Natrualight Home (Parent) v2.11
 
     Author:
             Brandon Spitza
@@ -13,9 +13,14 @@
         2.1 (2021-01-20)
             -Added timezone offset input to timePage which localizes default suggestions for period times
 
+        2.11 (2021-01-21)
+            -Modified welcome page description for further clarity
+            -Fixed: BUG: Turning off Alternate Dim State while lights are off turns lights on
+            -Fixed: BUG: Turning light off with a toggle button and Alternate Dim State active, lights don't turn back on (use Alternate Bright State on as workaround)
+            -Cleaned up some sloppy type-ing
+            -Cleaned up code
+
     To Do:
-            -BUG: Turning off Alternate Dim State while lights are off turns lights on
-            -BUG: Turning light off with a toggle button and Alternate Dim State active, lights don't turn back on (use Alternate Bright State on as workaround)
             -Validate user inputs
             -Collect least "next run time" from children and don't run until it for static periods
 
@@ -40,13 +45,13 @@ preferences {
     page(name: "appPage", title: "App Page", nextPage: "timePage", uninstall: true) {
         section("Select a button that, upon pressing, will correct any bulbs that have gone out of sync with their Group or Zone (if, for example, its " +
                 "lamp was momentarily unplugged). Consider a virtual button on a dashboard.") {
-            input "correctBulbStatesBtn", "capability.pushableButton", title: "Select Button to Correct Bulb States:", multiple: false, required: false
+            input name: "correctBulbStatesBtn", type: "capability.pushableButton", title: "Select Button to Correct Bulb States:", multiple: false, required: false
             paragraph ""
             paragraph ""
         }
         section("Set bulb update frequency. This is how often the app will check for and update bulb lighting settings. 3-4 minutes is recommended to " +
                 "reduce the perception of lighting changes.") {
-            input "updateFreq", "int", title: "Update Frequncy (min):", defaultValue: 3, required: true
+            input name: "updateFreq", type: "number", title: "Update Frequncy (min):", defaultValue: 3, required: true
             paragraph ""
             paragraph ""
         }
@@ -62,11 +67,11 @@ preferences {
         section("The warmest white temperature entered below will begin the Warm Morning period, fade to the coldest white temperature to start the Cold Morning period where " +
                 "it will stay set through the Day period. Into the Afternoon periods, white temperature will shift slowly back to the warmest white.") {
             paragraph ""
-            input "tempWarmest", "int", title: "Warmest CT:", defaultValue: 2500, required: true
+            input name: "tempWarmest", type: "number", title: "Warmest CT:", defaultValue: 2500, required: true
             paragraph "Hue bulb warmest CT: 2203"
             paragraph ""
             paragraph ""
-            input "tempColdest", "int", title: "Coldest CT:", defaultValue: 6300, required: true
+            input name: "tempColdest", type: "number", title: "Coldest CT:", defaultValue: 6300, required: true
             paragraph "Hue bulb coldest CT: 6536"
             paragraph ""
         }
@@ -79,79 +84,79 @@ preferences {
             paragraph "During the Early Evening period, Color Temperature Bulb levels have a separate setting than Color Bulb levels, allowing CT bulbs to fade independently " +
                 "of Color bulbs as overall lighting level needs decrease."
             paragraph ""
-            input "lvlInitWarmMorn", "int", title: "Warm Morning Period, Initial:", defaultValue: 40, required: true
+            input name: "lvlInitWarmMorn", type: "number", title: "Warm Morning Period, Initial:", defaultValue: 40, required: true
             paragraph ""
-            input "lvlGoalWarmMorn", "int", title: "Warm Morning Period, Goal:", defaultValue: 60, required: true
-            paragraph ""
-            paragraph ""
-            paragraph ""
-            
-            input "lvlGoalColdMorn", "int", title: "Cold Morning Period, Goal:", defaultValue: 100, required: true
+            input name: "lvlGoalWarmMorn", type: "number", title: "Warm Morning Period, Goal:", defaultValue: 60, required: true
             paragraph ""
             paragraph ""
             paragraph ""
             
-            input "lvlGoalDay", "int", title: "Day Period, Goal:", defaultValue: 100, required: true
+            input name: "lvlGoalColdMorn", type: "number", title: "Cold Morning Period, Goal:", defaultValue: 100, required: true
             paragraph ""
             paragraph ""
             paragraph ""
             
-            input "lvlGoalAfternoon", "int", title: "Afternoon Period, Goal:", defaultValue: 100, required: true
+            input name: "lvlGoalDay", type: "number", title: "Day Period, Goal:", defaultValue: 100, required: true
+            paragraph ""
+            paragraph ""
+            paragraph ""
+            
+            input name: "lvlGoalAfternoon", type: "number", title: "Afternoon Period, Goal:", defaultValue: 100, required: true
             paragraph ""
             paragraph ""
             paragraph ""
 
-            input "lvlCTGoalEveningCT", "int", title: "Early Evening Period, CT Bulb Goal:", defaultValue: 1, required: true
+            input name: "lvlCTGoalEveningCT", type: "number", title: "Early Evening Period, CT Bulb Goal:", defaultValue: 1, required: true
             paragraph ""
-            input "lvlCGoalEveningCT", "int", title: "Early Evening Period, Color Bulb Goal:", defaultValue: 100, required: true
-            paragraph ""
-            paragraph ""
-            paragraph ""
-
-            input "lvlCGoalEveningC", "int", title: "Late Evening Period, Color Bulb Goal:", defaultValue: 50, required: true
+            input name: "lvlCGoalEveningCT", type: "number", title: "Early Evening Period, Color Bulb Goal:", defaultValue: 100, required: true
             paragraph ""
             paragraph ""
             paragraph ""
 
-            input "lvlCGoalNight", "int", title: "Night Period, Color Bulb Goal:", defaultValue: 10, required: true
+            input name: "lvlCGoalEveningC", type: "number", title: "Late Evening Period, Color Bulb Goal:", defaultValue: 50, required: true
+            paragraph ""
+            paragraph ""
+            paragraph ""
+
+            input name: "lvlCGoalNight", type: "number", title: "Night Period, Color Bulb Goal:", defaultValue: 10, required: true
             paragraph ""
         }
     }
     page(name: "huePage", title: "Hue Settings", nextPage: "satPage", uninstall: true) {
         section("Set bulb hue range for each period.") {
             paragraph ""
-            input "hueInitEveningCT", "int", title: "Early Evening Period, Color Bulb Initial:", defaultValue: 11, required: true
+            input name: "hueInitEveningCT", type: "number", title: "Early Evening Period, Color Bulb Initial:", defaultValue: 11, required: true
             paragraph ""
-            input "hueGoalEveningCT", "int", title: "Early Evening Period, Color Bulb Goal:", defaultValue: 8, required: true
-            paragraph ""
-            paragraph ""
-            paragraph ""
-
-            input "hueGoalEveningC", "int", title: "Late Evening Period, Color Bulb Goal:", defaultValue: 5, required: true
+            input name: "hueGoalEveningCT", type: "number", title: "Early Evening Period, Color Bulb Goal:", defaultValue: 8, required: true
             paragraph ""
             paragraph ""
             paragraph ""
 
-            input "hueGoalNight", "int", title: "Night Period, Color Bulb Hue Goal:", defaultValue: 2, required: true
+            input name: "hueGoalEveningC", type: "number", title: "Late Evening Period, Color Bulb Goal:", defaultValue: 5, required: true
+            paragraph ""
+            paragraph ""
+            paragraph ""
+
+            input name: "hueGoalNight", type: "number", title: "Night Period, Color Bulb Hue Goal:", defaultValue: 2, required: true
             paragraph ""
         }
     }
     page(name: "satPage", title: "Saturation Settings", nextPage: "altBrightPage", uninstall: true) {
         section("Set bulb saturation range by period.") {
             paragraph ""
-            input "satInitEveningCT", "int", title: "Early Evening Period, Color Bulb Initial:", defaultValue: 70, required: true
+            input name: "satInitEveningCT", type: "number", title: "Early Evening Period, Color Bulb Initial:", defaultValue: 70, required: true
             paragraph ""
-            input "satGoalEveningCT", "int", title: "Early Evening Period, Color Bulb Goal:", defaultValue: 75, required: true
-            paragraph ""
-            paragraph ""
-            paragraph ""
-
-            input "satGoalEveningC", "int", title: "Late Evening Period, Color Bulb Saturion Goal:", defaultValue: 100, required: true
+            input name: "satGoalEveningCT", type: "number", title: "Early Evening Period, Color Bulb Goal:", defaultValue: 75, required: true
             paragraph ""
             paragraph ""
             paragraph ""
 
-            input "satGoalNight", "int", title: "Night Period, Color Bulb Goal:", defaultValue: 100, required: true
+            input name: "satGoalEveningC", type: "number", title: "Late Evening Period, Color Bulb Saturion Goal:", defaultValue: 100, required: true
+            paragraph ""
+            paragraph ""
+            paragraph ""
+
+            input name: "satGoalNight", type: "number", title: "Night Period, Color Bulb Goal:", defaultValue: 100, required: true
             paragraph ""
         }
     }
@@ -160,9 +165,9 @@ preferences {
                 "to the following settings until it is deactivated with a second button press/hold. Activating this state will turn on bulbs that are off. " +
                 "Like all lighting values, these can be refined at the room level if desired.") {
             paragraph ""
-            input "lvlAltStateBright", "int", title: "Alternate Bright Lighting Level:", defaultValue: 100, required: false
+            input name: "lvlAltStateBright", type: "number", title: "Alternate Bright Lighting Level:", defaultValue: 100, required: false
             paragraph ""
-            input "tempAltStateBright", "int", title: "Alternate Bright Lighting Temperature:", defaultValue: 2500, required: false
+            input name: "tempAltStateBright", type: "number", title: "Alternate Bright Lighting Temperature:", defaultValue: 2500, required: false
             paragraph ""
         }
     }
@@ -174,9 +179,9 @@ preferences {
                 "Color Temperature time period, i.e. prior to the Early Evening period. During Early Evening period and later, only the Lighting Level setting below applies while " +
                 "the bulb's other current states (hue, saturation) are maintained."
             paragraph ""
-            input "lvlAltStateDim", "int", title: "Alternate Dim Lighting Level:", defaultValue: 50, required: false
+            input name: "lvlAltStateDim", type: "number", title: "Alternate Dim Lighting Level:", defaultValue: 50, required: false
             paragraph ""
-            input "tempAltStateDim", "int", title: "Alternate Dim Lighting Temperature:", defaultValue: 2500, required: false
+            input name: "tempAltStateDim", type: "number", title: "Alternate Dim Lighting Temperature:", defaultValue: 2500, required: false
             paragraph ""
         }
     }
@@ -192,7 +197,7 @@ preferences {
 def welcomePage() {
     dynamicPage(name: "welcomePage") {
         section() {
-            input "appButtonPause", "button", title: "Un/Pause"
+            input name: "appButtonPause", type: "button", title: "Un/Pause"
             if (state.paused) { paragraph "***APP CURRENTLY PAUSED***" }
             paragraph ""
         }
@@ -201,7 +206,7 @@ def welcomePage() {
                 "reducing the perception of lighting changes. The design concept was as follows."
             paragraph "Early morning starts with warm white lighting, fades into cold white lighting as morning progresss, and transitions back to warm white lighting in the late " +
                 "afternoon. In the evening, bulbs that are only Color Temperature-adjustable are dimmed separately while Color bulb lighting levels stay high as they are switched " +
-                "to color mode and tinted slightly orange. As the evening continues, CT bulbs are switched off and Color bulbs become deeper orange."
+                "to color mode and tinted slightly orange. As the evening continues and lighting level needs decrease, CT bulbs are switched off and Color bulbs become deeper orange."
             paragraph "This scheme provides you with strong blue-shifted light during the day to promote wakefulness and improve mood. During the evening, " +
                 "your home's blue lighting is reduced and nearly eliminated, prompting your body to relax and increase melatonin production before you go to bed to aid in sleep quality. "
             paragraph "Naturalight allows you to intermingle Color bulbs with CT bulbs, which typically are half the cost. Why run 3 Color bulbs in a fixture at 30% " +
@@ -225,22 +230,21 @@ def timePage() {
                 "begins with the previous period's settings and fades into the next period's settings, completing the transition by the start time of the next period."
             paragraph ""
             paragraph ""
-            input "timezoneOffset", "int", title: "Enter your timezone offset to populate default period times (for reference: -5 for EST, -8 for PST)", required: false, submitOnChange: true
+            input name: "timezoneOffset", type: "number", title: "Enter your timezone offset to populate default period times (for reference: -5 for EST, -8 for PST)", required: false, submitOnChange: true
             if (timezoneOffset) {
-                timezoneOffset = Integer.parseInt(timezoneOffset) 
                 DecimalFormat fmt = new DecimalFormat("+00;-00")
                 strTimezoneOffset = fmt.format(timezoneOffset).toString()
             }
             paragraph ""
             paragraph ""
-            input "strTimeMornWarm", "time", title: "Warm Morning Start:", defaultValue: "2021-01-01T06:00:00.000${strTimezoneOffset}00", required: true
-            input "strTimeMornCold", "time", title: "Cold Morning Start:", defaultValue: "2021-01-01T06:45:00.000${strTimezoneOffset}00", required: true
-            input "strTimeDay", "time", title: "Day (Static) Start:", defaultValue: "2021-01-01T07:30:00.000${strTimezoneOffset}00", required: true
-            input "strTimeAfternoon", "time", title: "Early Afternoon Start:", defaultValue: "2021-01-01T15:30:00.000${strTimezoneOffset}00", required: true
-            input "strTimeAfternoonStatic", "time", title: "Late Afternoon (Static) Start:", defaultValue: "2021-01-01T17:30:00.000${strTimezoneOffset}00", required: true
-            input "strTimeEveningCT", "time", title: "Early Evening Start:", defaultValue: "2021-01-01T18:30:00.000${strTimezoneOffset}00", required: true
-            input "strTimeEveningC", "time", title: "Late Evening Start:", defaultValue: "2021-01-01T19:30:00.000${strTimezoneOffset}00", required: true
-            input "strTimeNight", "time", title: "Night (Static) Start:", defaultValue: "2021-01-01T22:15:00.000${strTimezoneOffset}00", required: true
+            input name: "strTimeMornWarm", type: "time", title: "Warm Morning Start:", defaultValue: "2021-01-01T06:00:00.000${strTimezoneOffset}00", required: true
+            input name: "strTimeMornCold", type: "time", title: "Cold Morning Start:", defaultValue: "2021-01-01T06:45:00.000${strTimezoneOffset}00", required: true
+            input name: "strTimeDay", type: "time", title: "Day (Static) Start:", defaultValue: "2021-01-01T07:30:00.000${strTimezoneOffset}00", required: true
+            input name: "strTimeAfternoon", type: "time", title: "Early Afternoon Start:", defaultValue: "2021-01-01T15:30:00.000${strTimezoneOffset}00", required: true
+            input name: "strTimeAfternoonStatic", type: "time", title: "Late Afternoon (Static) Start:", defaultValue: "2021-01-01T17:30:00.000${strTimezoneOffset}00", required: true
+            input name: "strTimeEveningCT", type: "time", title: "Early Evening Start:", defaultValue: "2021-01-01T18:30:00.000${strTimezoneOffset}00", required: true
+            input name: "strTimeEveningC", type: "time", title: "Late Evening Start:", defaultValue: "2021-01-01T19:30:00.000${strTimezoneOffset}00", required: true
+            input name: "strTimeNight", type: "time", title: "Night (Static) Start:", defaultValue: "2021-01-01T22:15:00.000${strTimezoneOffset}00", required: true
             paragraph ""
         }
     }
@@ -310,8 +314,6 @@ def getSatGoalEveningC(){ return satGoalEveningC }
 def getSatGoalNight(){ return satGoalNight }
 
 def driver(now) {
-    updateFreq = Integer.parseInt(updateFreq)
-    
     if (state.paused) {
         log("${app.label} app paused")
         log("Next state update ${updateFreq} minutes from now")
@@ -322,7 +324,6 @@ def driver(now) {
     if (!now) { now = new Date() }
     log("Scheduled bulb state update ${now}")
     
-    setPeriodTimes()
     
     def children = getChildApps()
     for (child in children) {
@@ -333,19 +334,6 @@ def driver(now) {
     
    log("Next state update ${updateFreq} minutes from now.")
    runIn(updateFreq * 60, driver)
-}
-
-def setPeriodTimes() {
-    log("${app.label}: Setting period times")
-    
-    timeMornWarm = toDateTime(strTimeMornWarm)
-    timeMornCold = toDateTime(strTimeMornCold)
-    timeDay = toDateTime(strTimeDay)
-    timeAfternoon = toDateTime(strTimeAfternoon)
-    timeAfternoonStatic = toDateTime(strTimeAfternoonStatic)
-    timeEveningCT = toDateTime(strTimeEveningCT)
-    timeEveningC = toDateTime(strTimeEveningC)
-    timeNight = toDateTime(strTimeNight)
 }
 
 def appButtonHandler(btn) {
